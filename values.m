525bf0a5e4d121d6c36bb8efa0190c8e1d860b0d
Rowan Jansens <rowan.jansens@gmail.com>
2023-03-06 16:19:29 -0500

    simulink model dev

--- values.m
+++ values.m
@@ -1,6 +1,26 @@
-t = 0.057;
+t = 0.1122;
 g = -9.8;
-K = 0.7931;
+K = 0.001589;
 m = 1.2;
 l = 0.5;
+
+
+% syms H(s) K tau v_step
+% % syms H(s) tau K
+% H = v_step/s*(K/tau)/(s+1/tau);
+% w = simplify(ilaplace(H))
+% v_step = 300;
+% w = subs(w)
+% K = 0.001589;
+% tau = 0.1122;
+% w = subs(w);
+% 
+% 
+% 
+% clf
+% fplot(w, [0 5]);
+% hold on
+% plot(time, left_motor)
+
+
 model
